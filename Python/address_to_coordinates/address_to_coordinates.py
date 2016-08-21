238c44acf0a1cb0597b0e5c8a7a261c4d836237c
Mike Bruno <mike.bruno@avantcredit.com>
2016-08-21 18:07:25 -0400

    address to coords

---
+++ Python/address_to_coordinates/address_to_coordinates.py
@@ -0,0 +1,30 @@
+# address_to_coordinates.py
+
+import geocoder
+import numpy
+import pdb
+import pandas
+
+
+
+def query_coordinates(address):
+  g = geocoder.google(address)
+  coordinates = g.latlng
+  return(coordinates)
+
+
+# import data
+address_csv_location = '/Users/mb/dev/one-off-analysis/Python/address_to_coordinates/test addresses.csv'
+
+#with open(address_csv_location, 'rb')
+
+address_data = numpy.recfromcsv(address_csv_location, delimiter = ',')
+
+df = pandas.read_csv(address_csv_location, sep = ',')
+
+df['query_address'] = df$address
+
+# query
+
+pdb.set_trace()
+
